f448439045df6657705665d5d1f4dfc479c3e5e3
skykwon1004 <118698700+skykwon1004@users.noreply.github.com>
2023-07-31 14:11:24 +0900

    Add files via upload

--- js/custom copy.js
+++ js/custom copy.js
@@ -8,20 +8,20 @@ $(function () {
         responsiveWidth: 700,
         //넘치는 부분 스크롤 하기.
         scrollOverflow: true,
+        //아랜 속도 부분... https://jqueryui.com/easing/ 참고.
+        // easing: 'easeOutBounce',
+        //easingCss3: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
         afterRender: function () {
             $('.Main__content .section').eq(0).addClass('on');
         },
-
         afterLoad: function (lnk, idx) {
-            console.log(idx);
+            console.log(lnk, idx);
             // $('.gnb li').eq(idx - 1).addClass('on').siblings().removeClass('on');
             $('.Main__content .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
-
-
         },
         onLeave: function (idx, nidx, dir) {
             $('.side_gnb li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
-            // console.log(idx, nidx, dir);
+            console.log(idx, nidx, dir);
 
             $('.gnb li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
 
@@ -33,36 +33,10 @@ $(function () {
         },
 
         afterLoad: function (anchorLink, index) {
-            console.log(index);
+            //console.log(index);
             index == 1 ? $('.toTop').fadeOut() : $('.toTop').fadeIn();
-
-            // 5번째 섹션에 도달했을 때 카운터를 시작하는 함수
-            if (index === 5) {
-                startCounter();
-            }
         },
-
     });
-
-
-    function startCounter() {
-        $('.counter').counterUp({
-            delay: 10,
-            time: 1000
-        });
-
-        $('.num').each(function () {
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 3000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        });
-    }
 
 
     // main_visual
@@ -136,6 +110,31 @@ $(function () {
     });
 
 
+
+
+
+
+
+    // counter
+    // $('.counter').counterUp({
+    //     delay: 10,
+    //     time: 1000
+    // });
+
+    // $('.num').each(function () {
+    //     $(this).prop('Counter', 0).animate({
+    //         Counter: $(this).text()
+    //     }, {
+    //         duration: 3000,
+    //         easing: 'swing',
+    //         step: function (now) {
+    //             $(this).text(Math.ceil(now));
+    //         }
+    //     });
+    // });
+
+
+
     // 4번째 섹션에 도달했을 때 카운터를 시작하는 함수
     function startCounter() {
         $('.counter').counterUp({
@@ -154,11 +153,17 @@ $(function () {
                 }
             });
         });
+
+        // 이벤트 리스너를 한 번만 실행한 후 제거합니다.
+        $(window).off('scroll', startCounter);
     }
 
-
-
-
+    // 4번째 섹션에 도달하면 startCounter 함수를 실행하는 이벤트 리스너 추가
+    $(window).on('scroll', function () {
+        if ($('.sub04').hasClass('active')) {
+            startCounter();
+        }
+    });
 
 
 
@@ -170,13 +175,21 @@ $(function () {
     //tab 기능 구현
     $('.left_itm .menu>li').on('click', function () {
         var idx = $(this).index();
-        // console.log(idx);
+        console.log(idx);
         $('.right_itm .content').removeClass('on');
         $('.right_itm .content').eq(idx).addClass('on');
 
         $('.left_itm .menu>li').removeClass('on');
         $(this).addClass('on');
     })
+
+
+
+
+
+
+
+
 
 
 
@@ -187,9 +200,6 @@ $(function () {
     // 더보기 버튼 클릭 시 추가 3개의 리스트 항목 보이기
     $("#load-more-btn").click(function () {
         $("#list-container a:hidden").slice(0, 4).show();
-
-        //높이를 100vh 씩 늘립니다.
-        $(".sub05 .inner").css("height", "+=400px");
 
         // 모든 리스트 항목이 보이면 더보기 버튼 숨기기
         if ($("#list-container a:hidden").length === 0) {
